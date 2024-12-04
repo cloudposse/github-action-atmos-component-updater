fbf6b79fc303f0c723f06009324e01154c751bf9
Igor Rodionov <goruha@gmail.com>
2024-12-04 15:10:39 +0100

    Test argocd

--- src/github_provider.py
+++ src/github_provider.py
@@ -93,6 +93,8 @@ class GitHubProvider:
             return
 
         repo = git.repo.Repo(repo_dir)
+        repo.index.add(repo.untracked_files)
+        git.add_all(repo_dir)
         diffs = repo.index.diff(None)
         tree_elements = []
         for d in diffs:
